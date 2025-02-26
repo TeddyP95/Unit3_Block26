97f777e447979e7f45faa4079c0a4a2d74ccdabf
Teddy Pillay <tpillay1995@gmail.com>
2025-02-25 20:43:21 -0500

    Contact rows file

--- Workshop/Unit3Block26/src/components/ContactList.jsx
+++ Workshop/Unit3Block26/src/components/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-
+import ContactRow from './ContactRow'
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -25,9 +25,9 @@ export default function ContactList() {
           <td>Email</td>
           <td>Phone</td>
         </tr>
-        {
-          // Map over data here
-        }
+        {contacts.map((contact) => (
+          <ContactRow key={contact.id} contact={contact} />
+        ))}
       </tbody>
     </table>
   ); 
