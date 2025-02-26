903f50ff63a07e7b1ba992a7203a41bcf518f6d5
Teddy Pillay <tpillay1995@gmail.com>
2025-02-25 20:52:29 -0500

    set up API

--- Workshop/Unit3Block26/src/components/ContactList.jsx
+++ Workshop/Unit3Block26/src/components/ContactList.jsx
@@ -1,5 +1,6 @@
-import React from "react"; 
+import React, { useState, useEffect } from "react"; 
 import ContactRow from './ContactRow'
+
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -10,6 +11,21 @@ const dummyContacts = [
 
 export default function ContactList() { 
     const [contacts, setContacts] = useState(dummyContacts)
+    
+    useEffect(() => {
+        async function fetchContacts() {
+            try {
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json();
+                console.log("API Response:", result);
+                setContacts(result);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchContacts();
+    }, []);
+
     console.log("Contacts: ", contacts)
 
   return ( 
