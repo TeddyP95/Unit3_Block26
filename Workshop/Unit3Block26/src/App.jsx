5120689b67cb51f161bbdbd2221499e4bfd7a1b7
Teddy Pillay <tpillay1995@gmail.com>
2025-02-25 20:36:55 -0500

    Contact List component

---
+++ Workshop/Unit3Block26/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useState } from 'react'
+import ContactList from './components/ContactList'
+
+function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  )
+}
+
+export default App
+
